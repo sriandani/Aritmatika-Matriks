be15f589f9c1b333a079b19c14bd17246f181f7f
sriandani <andanyzzz238@gmail.com>
2024-10-22 05:25:47 +0800

    Update AritmatikMatriks.py

--- AritmatikMatriks.py
+++ AritmatikMatriks.py
@@ -1,20 +1,20 @@
 baris_A=int(input("Masukkan ukuran baris matriks A= "))
 kolom_A=int(input("Masukkan ukuran kolom matriks A= "))
 
-baris_B=int(input("Masukkan ukuran baris matriks B= ")) 
+baris_B=int(input("Masukkan ukuran baris matriks B= "))
 kolom_B=int(input("Masukkan ukuran kolom matriks B= "))
 
-if (kolom_Al=baris_B):
-   print ("Matriks Tidak Dapat Dikalikan!")
+if (kolom_A!=baris_B):
+    print ("Matriks Tidak Dapat Dikalikan!")
 else:
-   print ("Masukkan element matriks A: ")
-   A=[]
-   for i in range (baris_A):
-       baris=[]
-       for j in range (kolom_A):
-           value=int(input("Masukkan element: ")) 
-           baris.append(value)
-       A.append(baris)
+    print ("Masukkan element matriks A : ")
+    A=[]
+    for i in range (baris_A):
+        baris=[]
+        for j in range (kolom_A):
+            value=int(input("Masukkan element: "))
+            baris.append(value)
+        A.append(baris)
     print("Masukkan element matriks B")
     B=[]
     for i in range (baris_B):
@@ -23,6 +23,6 @@ else:
             value=int(input("Masukkan element: "))
             baris.append(value)
         B.append(baris)
-
-print ("Matriks A: ", A)
+        
+print ("Matriks A : ", A)
 print ("Matriks B: ", B)
